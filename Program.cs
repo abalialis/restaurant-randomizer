7917266c20cbffc2daee5bf06ed1efe48252e95d
abalialis <68046673+abalialis@users.noreply.github.com>
2021-01-10 16:55:00 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace thing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You should eat at...");
+            
+            
+            Random rd = new Random();
+            string[] whereToEatAt = { "Whataburger", "Chic Fil Lay", "Taco Bell", "Panda Express" };
+            Console.WriteLine(whereToEatAt[rd.Next(0, 3)]);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("See you there!");
+            Console.ReadLine();
+        }
+    }
+}
